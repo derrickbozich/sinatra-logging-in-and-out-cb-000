0d6208b388476fad1c7c03b110354bcf116f5056
derrick b <dbozich1@gmail.com>
2018-07-24 07:22:12 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,14 +14,14 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(:username => params[:username])
     if @user != nil && @user.password == params[:password]
-      session[:user_id] = @user.id
+      session[:id] = @user.id
       redirect to '/account'
     end
     erb :error
   end
 
   get '/account' do
-    @current_user = User.find_by_id(session[:user_id])
+    @current_user = User.find_by_id(session[:id])
     if @current_user
       erb :account
     else
