088c6bb2a0fb27dd57627f60c15fc5217acbab10
derrick b <dbozich1@gmail.com>
2018-07-24 07:24:42 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
+require 'sinatra/base'
+
 class Helpers
+
+  def self.current_user(session)
+    binding.pry
+    @user = User.find_by_id(session[:id])
+  end
+
+  def self.is_logged_in?(session)
+    !!session[:id]
+  end
 end
