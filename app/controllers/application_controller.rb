7d4d180c2300d4cdd09f55ae1fde76260e61a9eb
derrick b <dbozich1@gmail.com>
2018-07-24 07:09:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,40 +1,36 @@
-require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
   configure do
-    set :views, Proc.new { File.join(root, "../views/") }
+    set :public_folder, 'public'
+    set :views, 'app/views'
     enable :sessions unless test?
     set :session_secret, "secret"
   end
 
   get '/' do
-
     erb :index
   end
 
   post '/login' do
-
-    @user = User.find_by(username: params[:username], password: params[:password])
-
-    if @user
-      session[:id] = @user.id
+    @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id
       redirect to '/account'
-
-    else
-      erb :error
     end
-
+    erb :error
   end
 
   get '/account' do
-    # @user = User.find_by_id(session[:id])
-    erb :account
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-    binding.pry
-    session = {}
-    redirect '/'
+    session.clear
+    redirect to '/'
   end
-
-
 end
